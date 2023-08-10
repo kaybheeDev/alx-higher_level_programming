54ff6424ea698feebcfa89509282f3d9bb12990c
abdulkabiropeyemi7@gmail.com <abdulkabiropeyemi7@gmail.com>
2023-08-10 12:24:41 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-    print("ASCII value: " + str(ch) + ", Character: ", chr(ch))
+for char in range(26):
+    print("{:s}".format(chr(char + ord("a"))), end="")
